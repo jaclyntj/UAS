1f47b1698e3cb8a1adb3b366b5782b5bda0b0a39
jaclyntj <03081190016@student.uph.edu>
2022-04-11 20:02:22 +0700

    Initial commit

---
+++ database/migrations/2022_04_11_114328_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('nama')->nullable();
+            $table->text('berita')->nullable();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
---
+++ resources/views/admin/event/form.blade.php
@@ -0,0 +1,15 @@
+<div class="form-group {{ $errors->has('nama') ? 'has-error' : ''}}">
+    <label for="nama" class="control-label">{{ 'Nama' }}</label>
+    <input class="form-control" name="nama" type="text" id="nama" value="{{ isset($event->nama) ? $event->nama : ''}}" >
+    {!! $errors->first('nama', '<p class="help-block">:message</p>') !!}
+</div>
+<div class="form-group {{ $errors->has('berita') ? 'has-error' : ''}}">
+    <label for="berita" class="control-label">{{ 'Berita' }}</label>
+    <textarea class="form-control" rows="5" name="berita" type="textarea" id="berita" >{{ isset($event->berita) ? $event->berita : ''}}</textarea>
+    {!! $errors->first('berita', '<p class="help-block">:message</p>') !!}
+</div>
+
+
+<div class="form-group">
+    <input class="btn btn-primary" type="submit" value="{{ $formMode === 'edit' ? 'Update' : 'Create' }}">
+</div>
